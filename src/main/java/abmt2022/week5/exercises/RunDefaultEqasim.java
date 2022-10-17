c6db13e3e0bcbab297f9648828f38fe88b3377ee
kaghog <kaghog@ivt-thkpd-69.mshome.net>
2022-10-17 17:53:47 +0200

    exercise template for week 5

---
+++ src/main/java/abmt2022/week5/exercises/RunDefaultEqasim.java
@@ -0,0 +1,21 @@
+package abmt2022.week5.exercises;
+
+public class RunDefaultEqasim {
+	
+	public static void main(String[] args)  {
+		
+		String configPath = args[0]; //use the sioux fall scenario config_default_eqasim.xml file
+		
+		//load the config file with the necessary modules (DMC module and eqasim...)
+		//Hint: Search for the EqasimConfigurator class and see what class we should load
+		
+		//load scenario
+		
+		//create controler
+		
+		//add modules to controler
+		
+		
+	}
+
+}
